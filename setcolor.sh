68f18395463cdc88cb07163e60ebe09f9262739f
brunotix <bruninho.ro@gmail.com>
2019-05-26 14:39:39 -0300

    Esse é um conceito de seletor de cores que fiz
    Está bem inicial ainda.

---
+++ setcolor.sh
@@ -0,0 +1,212 @@
+#!/bin/sh
+
+
+"$@"
+
+
+#Define a cor AZUL e suas INTENSIDADES:
+
+
+if [ "$@" = "blue1" ]; then
+   echo "Cor azul"
+   sudo python3 main.py -c blue -b 1
+fi
+
+if [ "$@" = "blue2" ]; then
+   echo "Cor azul"
+   sudo python3 main.py -c blue -b 2
+fi
+
+if [ "$@" = "blue3" ]; then
+   echo "Cor azul"
+   sudo python3 main.py -c blue -b 3
+fi
+
+if [ "$@" = "blue4" ]; then
+   echo "Cor azul"
+   sudo python3 main.py -c blue -b 4
+fi
+
+#Define a cor VERDE e suas INTENSIDADES:
+
+
+if [ "$@" = "green1" ]; then
+   echo "Cor verde"
+   sudo python3 main.py -c green -b 1
+fi
+
+if [ "$@" = "green2" ]; then
+   echo "Cor verde"
+   sudo python3 main.py -c green -b 2
+fi
+
+if [ "$@" = "green" ]; then
+   echo "Cor verde"
+   sudo python3 main.py -c green -b 3
+fi
+
+if [ "$@" = "green" ]; then
+   echo "Cor verde"
+   sudo python3 main.py -c green -b 4
+fi
+
+#Define a cor ROSA e suas INTENSIDADES:
+
+
+if [ "$@" = "pink1" ]; then
+   echo "Cor rosa"
+   sudo python3 main.py -c pink -b 1
+fi
+
+if [ "$@" = "pink2" ]; then
+   echo "Cor rosa"
+   sudo python3 main.py -c pink -b 2
+fi
+
+if [ "$@" = "pink3" ]; then
+   echo "Cor rosa"
+   sudo python3 main.py -c pink -b 3
+fi
+
+if [ "$@" = "pink4" ]; then
+   echo "Cor rosa"
+   sudo python3 main.py -c pink -b 4
+fi
+
+#Define a cor VERMELHA e suas INTENSIDADES:
+
+
+if [ "$@" = "red1" ]; then
+   echo "Cor vermelha"
+   sudo python3 main.py -c pink -b 1
+fi
+
+if [ "$@" = "red2" ]; then
+   echo "Cor vermelha"
+   sudo python3 main.py -c red -b 2
+fi
+
+if [ "$@" = "red3" ]; then
+   echo "Cor vermelha"
+   sudo python3 main.py -c red -b 3
+fi
+
+if [ "$@" = "red4" ]; then
+   echo "Cor vermelha"
+   sudo python3 main.py -c red -b 4
+fi
+
+#Define a cor AMARELA e suas INTENSIDADES:
+
+
+if [ "$@" = "yellow1" ]; then
+   echo "Cor amarela"
+   sudo python3 main.py -c yellow -b 1
+fi
+
+if [ "$@" = "yellow2" ]; then
+   echo "Cor amarela"
+   sudo python3 main.py -c yellow -b 2
+fi
+
+if [ "$@" = "yellow3" ]; then
+   echo "Cor amarela"
+   sudo python3 main.py -c yellow -b 3
+fi
+
+if [ "$@" = "yellow4" ]; then
+   echo "Cor amarela"
+   sudo python3 main.py -c yellow -b 4
+fi
+
+#Define a cor LARANJA/ABOBORA e suas INTENSIDADES:
+
+
+if [ "$@" = "orange1" ]; then
+   echo "Cor laranja/abobora"
+   sudo python3 main.py -c orange -b 1
+fi
+
+if [ "$@" = "orange2" ]; then
+   echo "Cor laranja/abobora"
+   sudo python3 main.py -c orange -b 2
+fi
+
+if [ "$@" = "orange3" ]; then
+   echo "Cor laranja/abobora"
+   sudo python3 main.py -c orange -b 3
+fi
+
+if [ "$@" = "orange4" ]; then
+   echo "Cor laranja/abobora"
+   sudo python3 main.py -c orange -b 4
+fi
+
+#Define a cor ROXO e suas INTENSIDADES:
+
+
+if [ "$@" = "purple1" ]; then
+   echo "Cor Roxa"
+   sudo python3 main.py -c purple -b 1
+fi
+
+if [ "$@" = "purple2" ]; then
+   echo "Cor Roxa"
+   sudo python3 main.py -c purple -b 2
+fi
+
+if [ "$@" = "purple3" ]; then
+   echo "Cor Roxa"
+   sudo python3 main.py -c purple -b 3
+fi
+
+if [ "$@" = "purple4" ]; then
+   echo "Cor Roxa"
+   sudo python3 main.py -c purple -b 4
+fi
+
+#Define a cor CIANO (AZUL CELESTE CLARO) e suas INTENSIDADES:
+
+
+if [ "$@" = "ciano1" ]; then
+   echo "Cor ciano (azul claro celeste)"
+   sudo python3 main.py -c teal -b 1
+fi
+
+if [ "$@" = "ciano2" ]; then
+   echo "Cor ciano (azul claro celeste)"
+   sudo python3 main.py -c teal -b 2
+fi
+
+if [ "$@" = "ciano3" ]; then
+   echo "Cor ciano (azul claro celeste)"
+   sudo python3 main.py -c teal -b 3
+fi
+
+if [ "$@" = "ciano4" ]; then
+   echo "Cor ciano (azul claro celeste)"
+   sudo python3 main.py -c teal -b 4
+fi
+
+#Define a cor BRANCO/LEVEMENTE AZULADO e suas INTENSIDADES:
+
+
+if [ "$@" = "branco1" ]; then
+   echo "Cor branca"
+   sudo python3 main.py -c white -b 1
+fi
+
+if [ "$@" = "branco2" ]; then
+   echo "Cor branca"
+   sudo python3 main.py -c white -b 2
+fi
+
+if [ "$@" = "branco3" ]; then
+   echo "Cor branca"
+   sudo python3 main.py -c white -b 3
+fi
+
+if [ "$@" = "branco4" ]; then
+   echo "Cor branca"
+   sudo python3 main.py -c white -b 4
+fi
